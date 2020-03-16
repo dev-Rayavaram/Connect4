7699fa7f04613cabe7ad4e5c558f0ea822049656
Student_M20 <student_m20@PS-DAL-M20.local>
2020-03-16 00:11:21 -0500

    app.js modified to check vertical and horizontal elements

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ $(()=>{
     let width,height,radius;
     let gameOver=false;
     let currentPlayer;
-    let board={
-
-    }
+    let gameObject=[];
+    let newGame=true;
     const drawCircle = (id,width,height,radius,color)=>{
         let c = document.getElementById(id);
         let ctx2 = c.getContext("2d");
@@ -15,6 +14,7 @@ $(()=>{
         ctx2.lineWidth = width/100;
         ctx2.strokeStyle = '#003300';
         ctx2.stroke();
+        gameOver=checkGameStatus(id);
     }
     const move=()=>{
         alert("move the piece");
@@ -23,42 +23,35 @@ $(()=>{
         alert("play the piece");
      
     }
-    const mouseENTER= function(){
-       // console.log("element");
-        
-        const col =$(this)[0].id;
-        if(currentPlayer === "player1"){
-            let element = document.getElementById(col);
-            console.log(element);
-                element.classList.remove('computer');
-                element.classList.add('player1');
-        }
-        else if(currentPlayer === "computer"){
-            let element = document.getElementById(col);
-                element.classList.remove('player1');
-                element.classList.add('computer');
-
-        }
-            
-      //  document.getElementById(col).setAttribute('class','hover')
-    }
-    const findLastFilledElement=(list,color)=>{
+     const findLastFilledElement=(list,color)=>{
        console.log("findLastFilledElement");
        for(let i=list.length-1;i>0;i--){
-            console.log("inside")
+            console.log("inside");
             let $cell = $(list[i]);
-            console.log("cell");
-            console.log($cell);
+          //  console.log("cell");
+           // console.log($cell);
 
             if($cell.hasClass('empty')){
                 let index= $($cell).attr('id');
-                console.log("index: "+index);
+             //   console.log("index: "+index);
+                newGame=false;
                 drawCircle(index,width,height,radius,color);
                 $cell.removeClass('empty');
+                $cell.addClass(`${currentPlayer}`);
                 if(currentPlayer ==="player1"){
                     currentPlayer ="computer";
+                    //create random event at random index
+                    let randomIndex = Math.floor((Math.random()*7));
+                    let randomId =`canvasCell[${randomIndex}][0]`;
+                 //   console.log("randomId");
+                //    console.log(randomId);
+                    let randomCell =document.getElementById(`${randomId}`);
+                    const mouseClickEvent = new Event('click');
+                    randomCell.dispatchEvent(mouseClickEvent);
+
                 }
                 else if(currentPlayer ==="computer"){
+                    
                     currentPlayer ="player1";
                 }
 
@@ -66,53 +59,263 @@ $(()=>{
         }
     }
 }
-    const checkGameStatus=()=>{
+const checkDiagonal=(colIndex,rowIndex)=>{
 
+}
+const checkHorizontal=(colIndex,rowIndex)=>{
+    gameObject=[];
+    let element = {};
+    if(!newGame){
+         let nextIndex=1;
+        let colInt=parseInt(colIndex);
+        let rowInt=parseInt(rowIndex);
+       // console.log("colInt"+rowInt);
+        element.col = colInt;
+        element.row = rowInt;
+        gameObject.push(element);
+
+        switch(colInt){
+            case 7:
+            //    console.log("case 7");
+                    for(let i=colInt-4;i<colInt;i++){
+                        let temp={};
+           //             console.log("inside for loop"+i);
+                        temp.col = i;
+                        temp.row = rowInt;
+                        gameObject.push(temp);
+                        nextIndex++; 
+                    }
+                break;
+ 
+            case 6:
+        //        console.log("case 6");
+                    for(let i=colInt-3;i<=colInt+1;i++){
+                        let temp={};
+        //                console.log("inside for loop"+i);
+                        temp.col = i;
+                        temp.row = rowInt;
+                        gameObject.push(temp);
+                        nextIndex++; 
+                    }
+                break;
+            case 5:
+        //        console.log("case 5");
+
+                for(let i=colInt-3;i<=colInt+2;i++){
+                    let temp={};
+        //            console.log("inside for loop"+i);
+                    temp.col = i;
+                    temp.row = rowInt;
+                    gameObject.push(temp);
+                     nextIndex++;              
+            }
+    
+                break;
+            case 4:
+        //        console.log("case 4");
+                    for(let i=colInt-3;i<=colInt+2;i++){
+                        let temp={};
+        //                console.log("inside for loop"+i);
+                        temp.col = i;
+                        temp.row = rowInt;
+                        gameObject.push(temp);
+                        nextIndex++;
+              
+                    }
+                   break;
+            case 3:
+        //        console.log("case 3");
+
+                    for(let i=colInt-2;i<=colInt+3;i++){
+                        let temp={};
+         //               console.log("inside for loop"+i);
+                        temp.col = i;
+                        temp.row = rowInt;
+                        gameObject.push(temp);
+                        nextIndex++;              
+                    }
+                   break;
+       
+                case 2:
+        //            console.log("case 2");
+
+                    for(let i=colInt-1;i<=colInt+3;i++){
+                        let temp={};
+
+        //                console.log("inside for loop"+i);
+                        temp.col = i;
+                        temp.row = rowInt;
+                        gameObject.push(temp);
+                        nextIndex++;              
+                    }
+                   break;
+       
+                   case 1:
+        //            console.log("case 1");
+
+                    for(let i=colInt;i<=colInt+3;i++){
+                        let temp={};
+        //                console.log("inside for loop"+i);
+                        temp.col = i;
+                        temp.row = rowInt;
+                        gameObject.push(temp);
+                        nextIndex++;              
+                    }
+                   break;
+                   default:
+                       break;  
+    
+            }
+    
+    //        console.log("gameObject is");
+    //        console.log(gameObject);
+     
+
+        }
+   
+}
+const checkVertical=(colIndex,rowIndex)=>{
+    gameObject=[];
+    let element = {};
+    if(!newGame){
+         let nextIndex=1;
+        let rowInt=parseInt(rowIndex);
+        let colInt=parseInt(colIndex);
+
+       // console.log("colInt"+rowInt);
+        element.col = colIndex;
+        element.row = rowInt;
+        gameObject.push(element);
+
+        switch(rowInt){
+            case 6:
+              //  console.log("case 6");
+                    for(let i=rowInt-3;i<=rowInt-1;i++){
+                        let temp={};
+                    //    console.log("inside for loop"+i);
+                        temp.col = colIndex;
+                        temp.row = i;
+                        gameObject.push(temp);
+                        nextIndex++; 
+                    }
+                break;
+            case 5:
+            //    console.log("case 5");
+
+                for(let i=rowInt-3;i<=rowInt+1;i++){
+                    let temp={};
+            //        console.log("inside for loop"+i);
+                    temp.col = colIndex;
+                    temp.row = i;
+                    gameObject.push(temp);
+                     nextIndex++;              
+            }
+    
+                break;
+            case 4:
+            //    console.log("case 4");
+                    for(let i=rowInt-3;i<=rowInt+2;i++){
+                        let temp={};
+            //            console.log("inside for loop"+i);
+                        temp.col = colIndex;
+                        temp.row = i;
+                        gameObject.push(temp);
+                        nextIndex++;
+              
+                    }
+                   break;
+            case 3:
+            //    console.log("case 3");
+
+                    for(let i=rowInt-2;i<=rowInt+3;i++){
+                        let temp={};
+                //        console.log("inside for loop"+i);
+                        temp.col = colIndex;
+                        temp.row = i;
+                        gameObject.push(temp);
+                        nextIndex++;              
+                    }
+                   break;
+       
+                case 2:
+                //    console.log("case 2");
+
+                    for(let i=rowInt-1;i<=rowInt+3;i++){
+                        let temp={};
+
+                //        console.log("inside for loop"+i);
+                        temp.col = colIndex;
+                        temp.row = i;
+                        gameObject.push(temp);
+                        nextIndex++;              
+                    }
+                   break;
+       
+                   case 1:
+            //        console.log("case 1");
+
+                    for(let i=rowInt;i<=rowInt+3;i++){
+                        let temp={};
+            //            console.log("inside for loop"+i);
+                        temp.col = colIndex;
+                        temp.row = i;
+                        gameObject.push(temp);
+                        nextIndex++;              
+                    }
+                   break;
+                   default:
+                       break;  
+    
+            }
+    
+        //    console.log("gameObject is");
+         //   console.log(gameObject);
+     
+
+        }
+     }
+ 
+    
+    const checkGameStatus=(index)=>{
+        console.log("inside checkGameStatus");
+        const colData = document.getElementById(index).getAttribute('col');
+        const rowData = document.getElementById(index).getAttribute('row'); 
+       // console.log(`colData ${colData} rowData ${rowData}`);
+        checkVertical(colData,rowData);
+        checkHorizontal(colData,rowData);
+        checkDiagonal(colData,rowData);   
+        return false;
     }
     const game=(event)=>{
-        console.log(event);
+        //console.log(event);
         const colData = $(event).attr('col');
         const rowData = $(event).attr('row');
         let columnsList = $(`.col${colData}`);
-        console.log(columnsList);
+       // console.log(columnsList);
         if(currentPlayer ==="player1"){
             findLastFilledElement(columnsList,'red');
         }
         else{
             findLastFilledElement(columnsList,'yellow');
         }
-        checkGameStatus();
-
-      
 
     }
     const mouseClick= function(){
+        event.preventDefault();
+
          game(this);
       //  console.log(col);
       //  drawCircle(col,width,height,radius,'red');
     }
-    const mouseOUT= function(){
-        const col =$(this)[0].id;
-       // document.getElementById(col).setAttribute('class','hidden')
-       // drawCircle(col,width,height,radius,'hidden');
-
-    }
-   const start=()=>{
+    const start=()=>{
        console.log("inside start");
        const $board =$('.col');
-       $board.on('mouseover',mouseENTER);
-       $board.on('click',mouseClick);
-
-       $board.on('mouseout',mouseOUT);
-    
+       $board.on('click',mouseClick);    
    } 
    const stop=()=>{
     console.log("inside stop");
     const $board =$('.col');
-    $board.off('mouseenter',mouseENTER);
     $board.off('click',mouseClick);
-
-    $board.off('mouseout',mouseOUT);
    }
 const initializeBoard=()=>{
  
